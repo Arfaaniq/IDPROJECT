5385724118716d7560c4ddb34b47ab173bde4ed7
Betterniq <arfaaniq474@gmail.com>
2025-07-18 14:52:31 +0700

    rapikan route

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -1,15 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\Auth\LoginController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Admin\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Auth\LoginController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\KalenderController;
@@ -19,88 +12,58 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\InstagramEmbedController;
 
+// Admin Authentication Routes (Guest)
 Route::prefix('admin')->middleware('guest:admin')->group(function () {
     Route::get('login', [LoginController::class, 'create'])->name('admin.login');
     Route::post('login', [LoginController::class, 'store']);
-
-    // Route register jika diperlukan
+    
     Route::get('register', [RegisteredUserController::class, 'create'])->name('admin.register');
     Route::post('register', [RegisteredUserController::class, 'store']);
-
-    // Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])->name('password.request');
-    // Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])->name('password.email');
-    // Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])->name('password.reset');
-    // Route::post('reset-password', [NewPasswordController::class, 'store'])->name('password.store');
 });
 
+// Admin Authenticated Routes
 Route::prefix('admin')->middleware('auth:admin')->group(function () {
-
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-
+    // Dashboard
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-
-
-    // Route::get('verify-email', EmailVerificationPromptController::class)->name('verification.notice');
-    // Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)->middleware(['signed', 'throttle:6,1'])->name('verification.verify');
-    // Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    //             ->middleware('throttle:6,1')
-    //             ->name('verification.send');
-    // Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])->name('password.confirm');
-
-    // Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
-
-    // Route::put('password', [PasswordController::class, 'update'])->name('password.update');
-    // admin.dashboard
-
+    
+    // Profile Management
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    //Riwayat
+    
+    // Verification Routes
+    Route::get('/verifikasi', [VerifyController::class, 'index'])->name('verifikasi');
+    Route::post('/verifikasi/{id}/onProgress', [VerifyController::class, 'onProgress'])->name('verifikasi.onProgress');
+    Route::post('/verifikasi/{id}/finish', [VerifyController::class, 'finish'])->name('verifikasi.finish');
+    Route::post('/verifikasi/cancel/{id}', [VerifyController::class, 'cancel'])->name('verifikasi.cancel');
+    Route::post('/verifikasi/{id}/approve', [VerifyController::class, 'approve'])->name('verifikasi.approve');
+    Route::post('/verifikasi/{id}/reject', [VerifyController::class, 'reject'])->name('verifikasi.reject');
+    
+    // History Routes
     Route::get('/riwayat', [RiwayatController::class, 'index'])->name('riwayat');
     Route::post('/riwayat/upload-invoice/{id}', [RiwayatController::class, 'uploadInvoice'])->name('riwayat.upload.invoice');
-
-    // routes kalender yang baru dibuat
-    Route::get('/events-all', [EventController::class, 'listAllEvents'])->name('events.all'); // route baru untuk filter penambahan
+    
+    // Event Management
+    Route::get('/events-all', [EventController::class, 'listAllEvents'])->name('events.all');
     Route::get('/events/list', [EventController::class, 'listEvent'])->name('events.list');
     Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
     Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
     Route::resource('events', EventController::class);
-    // Route::get('kalender', [KalenderController::class, 'index'])->name('kalender.index');
-    // routes kalender list eventnya
-    // Route::get('/kalenders/list',[KalenderController::class, 'listKalender'])->name('kalenders.list');
-    // Route::get('/kalenders/create', [KalenderController::class, 'create'])->name('kalenders.create');
-    // Route::post('/kalenders/store', [KalenderController::class, 'store'])->name('kalenders.store');
-
-    Route::get('/verifikasi', [VerifyController::class, 'index'])->name('verifikasi');
-    Route::post('/verifikasi/{id}/onProgress', [VerifyController::class, 'onProgress'])->name('verifikasi.onProgress');
-    // Route untuk menandai verifikasi sebagai selesai
-    Route::post('/verifikasi/{id}/finish', [VerifyController::class, 'finish'])->name('verifikasi.finish');
-    Route::post('/verifikasi/cancel/{id}', [VerifyController::class, 'cancel'])->name('verifikasi.cancel');
-    Route::get('/verifikasi', [VerifyController::class, 'index'])->name('verifikasi');
-    Route::post('/verifikasi/{id}/approve', [VerifyController::class, 'approve'])->name('verifikasi.approve');
-    Route::post('/verifikasi/{id}/reject', [VerifyController::class, 'reject'])->name('verifikasi.reject');
-
+    
+    // Project Management
     Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
     Route::get('/projects.index', [ProjectController::class, 'index'])->name('projects.index');
     Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
     Route::post('/projects/add', [ProjectController::class, 'store'])->name('projects.store');
     Route::delete('/projects/{project}/delete', [ProjectController::class, 'destroy'])->name('projects.destroy');
-
-    // routes kalender events
-    // Route::get('/events/list', [EventController::class, 'listEvent'])->name('events.list');
-    // Route::get('/events/create', [EventController::class, 'create'])->name('events.create');
-    // Route::post('/events/store', [EventController::class, 'store'])->name('events.store');
-    // Route::resource('events', EventController::class);
-
+    
+    // Instagram Embed Management
     Route::get('/instagram', [InstagramEmbedController::class, 'index'])->name('instagram.index');
     Route::post('/instagram', [InstagramEmbedController::class, 'store'])->name('instagram.store');
     Route::get('/instagram/{embed}/edit', [InstagramEmbedController::class, 'edit'])->name('instagram.edit');
     Route::put('/instagram/{embed}', [InstagramEmbedController::class, 'update'])->name('instagram.update');
     Route::delete('/instagram/{embed}', [InstagramEmbedController::class, 'destroy'])->name('instagram.destroy');
-
-    Route::post('logout', [LoginController::class, 'destroy'])
-        ->name('admin.logout');
+    
+    // Logout
+    Route::post('logout', [LoginController::class, 'destroy'])->name('admin.logout');
 });
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,25 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\IdProject;
-use App\Http\Controllers\VerifyController;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Admin\HomeUserController;
-use App\Http\Controllers\Admin\AboutController;
-use App\Http\Controllers\Admin\ServiceController;
-use App\Http\Controllers\Admin\BlogController;
-use App\Http\Controllers\Admin\BlogdetailController;
-use App\Http\Controllers\Admin\ContactController;
-use App\Http\Controllers\EventController;
-use App\Http\Controllers\BlogAdminController;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\CustomerProjectController;
-use App\Http\Controllers\InstagramEmbedController;
+use App\Http\Controllers\{
+    ProfileController,
+    VerifyController,
+    CustomerController,
+    AuthController,
+    EventController,
+    CommentController,
+    CustomerProjectController,
+    InstagramEmbedController,
+    BlogAdminController
+};
+use App\Http\Controllers\Admin\{
+    HomeUserController,
+    AboutController,
+    ServiceController,
+    BlogController,
+    BlogdetailController,
+    ContactController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -30,85 +32,79 @@ use App\Http\Controllers\InstagramEmbedController;
 |
 */
 
+// Public Routes
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
 
-Route::get('/', function () { return view('welcome'); })->name('welcome');
 Route::get('/home', [HomeUserController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/Service', [ServiceController::class, 'index'])->name('service');
-Route::get('/Blog', [BlogController::class, 'index'])->name('blog');
-Route::get('/BlogDetail', [BlogDetailController::class, 'index'])->name('blogdetail');
 Route::get('/Contact', [ContactController::class, 'index'])->name('contact');
-Route::get('/projects', [CustomerProjectController::class, 'index'])->name('customer.projects');
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
 
-
-
-Route::prefix('admin')->middleware('auth:admin')->name('admin.')->group(function () {
-    Route::resource('blog', BlogAdminController::class);
+// Blog Routes
+Route::prefix('blog')->group(function () {
+    Route::get('/', [BlogController::class, 'index'])->name('blog');
+    Route::get('/{id}', [BlogController::class, 'show'])->name('blog.show');
+    Route::get('/search', [BlogController::class, 'search'])->name('blog.search');
+    Route::post('/{blog}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/detail', [BlogdetailController::class, 'index'])->name('blogdetail');
 });
-Route::get('/blog', [BlogController::class, 'index'])->name('blog.index');
-Route::get('/blog/{id}', [BlogController::class, 'show'])->name('blog.show');
-Route::get('/blog-search', [BlogController::class, 'search'])->name('blog.search');
-Route::post('/comments/{blog}', [CommentController::class, 'store'])->name('comments.store');
 
-
+// Authentication Routes
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
-// Di routes/web.php
-Route::prefix('admin')->middleware('auth:admin')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard');
-    })->name('admin.dashboard');
-
-    // ... route admin lainnya
-});
-// Route::get('/', function () {
-//     Mail::to('arfaaniqsabila474@gmail.com')
-//     ->send(new IdProject());
-//     return view('auth.login');
-// });
-
-// Route untuk menampilkan form (GET)
+// Authenticated User Routes
 Route::middleware('auth:web')->group(function () {
-    Route::get('/orderservice', [VerifyController::class, 'showOrderForm'])->name('orderservice.form');
-    Route::post('/orderservice', [VerifyController::class, 'order'])->name('orderservice');
+    Route::prefix('orderservice')->group(function () {
+        Route::get('/', [VerifyController::class, 'showOrderForm'])->name('orderservice.form');
+        Route::post('/', [VerifyController::class, 'order'])->name('orderservice');
+    });
+    
+    Route::get('/projects', [CustomerProjectController::class, 'index'])->name('customer.projects');
+    Route::get('/customers/status', [CustomerController::class, 'status'])->name('customer/status');
 });
 
-//mail
-Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
-
-//Embed IG
-Route::prefix('admin')->name('admin.')->group(function () {
-    Route::get('/instagram', [InstagramEmbedController::class, 'index'])->name('instagram.index');
-    Route::post('/instagram', [InstagramEmbedController::class, 'store'])->name('instagram.store');
-    Route::get('/instagram/{embed}/edit', [InstagramEmbedController::class, 'edit']) ->name('instagram.edit');
-    Route::put('/instagram/{embed}', [InstagramEmbedController::class, 'update'])->name('instagram.update');
-    Route::delete('/instagram/{embed}', [InstagramEmbedController::class, 'destroy'])->name('instagram.destroy');
-});
-
-// Setelah login akan masuk ke halaman verifikasi
-
-// Halaman welcome setelah login
-Route::get('/orderservice', [AuthController::class, 'welcome'])->name('orderservice')->middleware('auth');
-
-// bahasa
+// Language Route
 Route::get('/lang/{locale}', function ($locale) {
-    if (! in_array($locale, ['id', 'en'])) abort(400);
+    if (!in_array($locale, ['id', 'en'])) abort(400);
     session(['locale' => $locale]);
     return redirect()->back();
 })->name('change.language');
 
+// Admin Routes
+Route::prefix('admin')->name('admin.')->group(function () {
+    // Public Admin Routes (login/register)
+    Route::middleware('guest:admin')->group(function () {
+        // These would typically be in admin-auth.php
+    });
 
+    // Authenticated Admin Routes
+    Route::middleware('auth:admin')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('admin.dashboard');
+        })->name('dashboard');
+        
+        Route::resource('blog', BlogAdminController::class);
+        
+        Route::prefix('instagram')->group(function () {
+            Route::get('/', [InstagramEmbedController::class, 'index'])->name('instagram.index');
+            Route::post('/', [InstagramEmbedController::class, 'store'])->name('instagram.store');
+            Route::get('/{embed}/edit', [InstagramEmbedController::class, 'edit'])->name('instagram.edit');
+            Route::put('/{embed}', [InstagramEmbedController::class, 'update'])->name('instagram.update');
+            Route::delete('/{embed}', [InstagramEmbedController::class, 'destroy'])->name('instagram.destroy');
+        });
+        
+        Route::put('/history/{id}/notes', [App\Http\Controllers\RiwayatController::class, 'updateNotes'])
+            ->name('history.update.notes');
+            
+        Route::get('/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
+    });
+});
 
-//Customer status
-Route::get('/customers/status', [CustomerController::class, 'status'])->name('customer/status')->middleware('auth.login');
-
-Route::put('/admin/history/{id}/notes', [App\Http\Controllers\RiwayatController::class, 'updateNotes'])->name('history.update.notes');
-
-Route::get('/admin/events/{event}/edit', [EventController::class, 'edit'])->name('events.edit');
-
+// Include Auth Routes
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin-auth.php';
-
-// bismillah 12
